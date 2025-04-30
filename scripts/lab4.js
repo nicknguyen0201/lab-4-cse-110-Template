c4f1ee5e9254aeb8bcf3cba73b4ec596aec39d8b
Nick Nguyen <nin002@ucsd.edu>
2025-04-30 00:57:31 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -27,12 +27,14 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
+    if(length===0) return false;
+    
     for(let i = 0; i < length; i++) {
+        let discountedPrice = 0
         discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
-
+    
     return discounted;
 }
 
